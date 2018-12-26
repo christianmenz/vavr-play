df7bbcbd675943bfba44da7744920b31762befd5
Christian Menz <christianmenz@gmail.com>
2018-12-26 12:05:35 +0100

    extended examples

--- src/main/java/ch/christianmenz/vavr/TryExample.java
+++ src/main/java/ch/christianmenz/vavr/TryExample.java
@@ -9,11 +9,19 @@ import java.util.stream.Collectors;
 public class TryExample {
 
     public static void main(String[] args) {
+        // Simple Try example
+        System.out.println("--- simple try example ---");
         Try<Integer> test = Try.of(() -> convert("test"));
         System.out.println(test.isSuccess());
         System.out.println(test.isFailure());
         System.out.println(test.getOrElse(0));
-        List.of("hello", "1", "2", "world").stream().map(i -> Try.of(() -> TryExample.convert(i))).collect(Collectors.toList());
+
+        // Try of in a stream
+        System.out.println("--- try of in a stream ---");
+        List.of("hello", "1", "2", "world").stream().map(i -> Try.of(() -> TryExample.convert(i))).map(o -> o.getOrElse(-1)).forEach(System.out::println);
+
+        // Using checked function
+        System.out.println("--- using checked function ---");
         List.of("helllo", "1", "2").stream().map(CheckedFunction1.lift(i -> convert((i)))).forEach(o -> {
             System.out.println(o.getOrElse(-1));
         });
