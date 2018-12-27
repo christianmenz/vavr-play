f9c0e11764b1abe7107f6c43cd7f1bbcc3f5f19a
Christian Menz <christianmenz@gmail.com>
2018-12-27 14:22:27 +0100

    example with parallel

--- src/main/java/ch/christianmenz/vavr/Main.java
+++ src/main/java/ch/christianmenz/vavr/Main.java
@@ -2,8 +2,10 @@ package ch.christianmenz.vavr;
 
 
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -11,6 +13,7 @@ public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         long start = System.currentTimeMillis();
         ForkJoinPool forkJoinPool = new ForkJoinPool(4);
+        // The main thread submits the task to the pool, is this the reason main is used?
         java.util.List<Integer> collect = forkJoinPool.submit(() -> List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15).stream().parallel().map(i -> {
             System.out.println(Thread.currentThread().getName());
             return i;
@@ -18,6 +21,24 @@ public class Main {
 
         System.out.println("time: " + (System.currentTimeMillis() - start));
         System.out.println(collect);
+
+        // Let's check if the whole stuff is submitted by another thread
+
+        System.out.println("---- ");
+
+        Callable<Object> callable = new Callable<Object>() {
+            public Object call() throws Exception {
+                java.util.List<Integer> collect = forkJoinPool.submit(() -> List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15).stream().parallel().map(i -> {
+                    System.out.println(Thread.currentThread().getName());
+                    return i;
+                })).get().collect(Collectors.toList());
+                return null;
+            }
+        };
+        ForkJoinTask<Object> submit = forkJoinPool.submit(callable);// use the pool to submit...
+
+        // wait for termination of the task if needed..
+        submit.get();
 
     }
 }
